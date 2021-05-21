3c92cc35029bbf89810a62ea8c7ad423156b88dd
PraZ <sachabouillez@gmail.com>
2021-05-21 19:19:58 +0200

    :tada: initial commit

---
+++ lib/index.ts
@@ -0,0 +1,86 @@
+import { useTimestamp, useStorage } from '@vueuse/core'
+import { computed, ComputedRef } from 'vue-demi'
+
+export interface StopwatchOptions {
+  /**
+   * Start stopwatch as composable is called.
+   * @default false
+   */
+  immediate?: boolean
+}
+
+export interface StopwatchReturn {
+  /**
+   * Pauses the stopwatch.
+   */
+  pause: () => void
+  /**
+   * Resumes the stopwatch.
+   */
+  resume: () => void
+  /**
+   * Resets the stopwatch to 0.
+   */
+  reset: () => void
+  /**
+   * Whether the stopwatch is running or not.
+   */
+  running: ComputedRef<boolean>
+  /**
+   * Time elapsed in milliseconds.
+   */
+  elapsed: ComputedRef<number>
+}
+
+/**
+ * Stopwatch persisted in storage.
+ * @param key Key to reference the stopwatch in storage.
+ * @param options
+ */
+export default function useStopwatch(key: string, options: StopwatchOptions = {}): StopwatchReturn {
+  const { immediate = false } = options
+
+  const { timestamp: now } = useTimestamp()
+
+  const startTimestamp = useStorage(`stopwatch-timestamp-${key}`, now.value)
+  const pauseTimestamp = useStorage(`stopwatch-pause-${key}`, immediate ? 0 : now.value)
+  const offset = useStorage(`stopwatch-offset${key}`, 0)
+  
+
+  const running = computed(() => !pauseTimestamp.value)
+  const currentPause = computed(() => !running.value ? now.value - pauseTimestamp.value : 0)
+  const elapsed = computed(() => now.value - startTimestamp.value - offset.value - currentPause.value)
+
+  /**
+   * Pauses the stopwatch.
+   */
+  function pause() {
+    if (!running) return
+    pauseTimestamp.value = now.value
+  }
+
+  /**
+   * Resumes the stopwatch.
+   */
+  function resume() {
+    offset.value += currentPause.value
+    pauseTimestamp.value = 0
+  }
+
+  /**
+   * Resets the stopwatch to 0.
+   */
+  function reset() {
+    startTimestamp.value = now.value
+    offset.value = 0
+    if (!running.value) pauseTimestamp.value = now.value
+  }
+
+  return {
+    pause,
+    resume,
+    reset,
+    elapsed,
+    running
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import useStopwatch from '../dist/index'
+
+const { elapsed, pause, resume, reset,running } = useStopwatch('library')
+</script>
+  
+<template>
+  <div>elapsed time: {{ elapsed }}</div>
+  <div>running: {{ running }}</div>
+  <button @click="resume">resume</button>
+  <button @click="pause">pause</button>
+  <button @click="reset">reset</button>
+</template>
