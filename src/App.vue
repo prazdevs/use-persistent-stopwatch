0d622257fbef4864e86fe0924640b7480e4c78da
PraZ <sachabouillez@gmail.com>
2021-05-21 21:33:45 +0200

    :art: clean demo app code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import useStopwatch from '../dist/index'
 
-const { elapsed, pause, resume, reset,running } = useStopwatch('library')
+const { elapsed, pause, resume, reset, running } = useStopwatch('library')
 </script>
   
 <template>
-  <div>elapsed time: {{ elapsed }}</div>
-  <div>running: {{ running }}</div>
-  <button @click="resume">resume</button>
-  <button @click="pause">pause</button>
-  <button @click="reset">reset</button>
+  <div>
+    <span :style="{ color: running ? 'green' : 'red' }">elapsed time: {{ elapsed }}</span>
+    <button @click="resume">resume</button>
+    <button @click="pause">pause</button>
+    <button @click="reset">reset</button>
+  </div>
 </template>
