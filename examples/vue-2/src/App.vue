e408266727f943d6737232c7ce67f386c845fd17
PraZ <sachabouillez@gmail.com>
2021-05-22 22:16:02 +0200

    :memo: add examples

---
+++ examples/vue-2/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { defineComponent } from '@vue/composition-api'
+import usePersistentStopwatch from 'use-persistent-stopwatch'
+
+export default defineComponent({
+  setup() {
+    const { elapsed, running, resume, pause, reset } = usePersistentStopwatch('cute-watch-2')
+
+    return { elapsed, running, resume, pause, reset }
+  }
+})
+</script>
+
+<template>
+  <div>
+    <span :style="{ color: running ? 'green' : 'red' }">{{ elapsed }}</span>
+    <button @click="resume">resume</button>
+    <button @click="pause">pause</button>
+    <button @click="reset">reset</button>
+  </div>
+</template>
---
+++ examples/vue-3/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import usePersistentStopwatch from 'use-persistent-stopwatch'
+
+const { elapsed, running, resume, pause, reset } = usePersistentStopwatch('cute-watch-3')
+</script>
+
+<template>
+  <span :style="{ color: running ? 'green' : 'red' }">
+    {{ elapsed }}
+  </span>
+  <button @click="resume">resume</button>
+  <button @click="pause">pause</button>
+  <button @click="reset">reset</button>
+</template>
